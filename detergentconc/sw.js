45dd57d8d30b3c165ef209ad6958f1e1dde95500
grahamm89 <116651536+grahamm89@users.noreply.github.com>
2025-07-24 17:29:02 +0100

    Update sw.js

--- detergentconc/sw.js
+++ detergentconc/sw.js
@@ -1,1 +1,10 @@
+const CACHE='detergent-v1';
+const CORE=['./','index.html','main.css','main.js','detergents.json','manifest.webmanifest','icon-192.png','icon-512.png'];
 
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
